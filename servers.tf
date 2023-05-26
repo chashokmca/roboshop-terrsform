a4501154dc26f859d0b2eefe80481bd0dfb23bda
chashokmca <chashokmca@gmail.com>
2023-05-26 23:23:51 +0530

    Initial Draft

--- servers.tf
+++ servers.tf
@@ -1,98 +1,35 @@
+variable "components" {
+  default = ["frontend", "mongodb"]
+}
+
+data "aws_ami" "centos" {
+  owners = ["973714476881"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+}
+
 resource "aws_instance" "frontend" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
+  count   = length(var.components)
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "frontend"
+    Name = var.components[count]
   }
 }
 
-resource "aws_instance" "mongodb" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "mongodb"
-  }
+resource "aws_route53_record" "frontend" {
+  name    = "frontend-dev.adevops72.online"
+  type    = "A"
+  zone_id = "Z064328134L0JD7T8XRZ9"
+  ttl = 30
+  records = [aws_instance.frontend.private_ip]
 }
 
-resource "aws_instance" "catalogue" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "catalogue"
-  }
+output "public_ip" {
+  value = aws_instance.frontend.public_ip
 }
 
-resource "aws_instance" "cart" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "cart"
-  }
-}
-
-resource "aws_instance" "mysql" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_instance" "payment" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "payment"
-  }
-}
-
-resource "aws_instance" "rabbitmq" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-
-resource "aws_instance" "redis" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "redis"
-  }
-}
-
-resource "aws_instance" "shipping" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_instance" "user" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "user"
-  }
-}
-
-resource "aws_instance" "dispatch" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "dispatch"
-  }
+output "private_ip" {
+  value = aws_instance.frontend.private_ip
 }
