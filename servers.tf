2724831e01f95f10327dabf8be0f8b0ea8791ce2
chashokmca <chashokmca@gmail.com>
2023-05-27 15:53:17 +0530

    Initial Draft

--- servers.tf
+++ servers.tf
@@ -1,5 +1,5 @@
 variable "components" {
-  default = ["frontend", "mongodb"]
+  default = ["frontend", "mongodb", "catalogue"]
 }
 
 data "aws_ami" "centos" {
@@ -16,27 +16,27 @@ variable "instance_type" {
   default = "t3.micro"
 }
 
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
   count   = length(var.components)
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
 
   tags = {
-    Name = var.components[count]
+    Name = var.components[count.index]
   }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "instance" {
   name    = "frontend-dev.adevops72.online"
   type    = "A"
   zone_id = "Z064328134L0JD7T8XRZ9"
   ttl = 30
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.instance.private_ip]
 }
 
 output "public_ip" {
-  value = aws_instance.frontend.public_ip
+  value = aws_instance.instance.public_ip
 }
 
 output "private_ip" {
