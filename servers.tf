91bb899891123d8115fd26f53eb4224585c2cb84
chashokmca <chashokmca@gmail.com>
2023-05-27 13:09:01 +0530

    Initial Draft

--- servers.tf
+++ servers.tf
@@ -8,10 +8,19 @@ data "aws_ami" "centos" {
   name_regex = "Centos-8-DevOps-Practice"
 }
 
+data "aws_security_group" "selected" {
+  id = "sg-0d5c740b322bfa5ca"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
 resource "aws_instance" "frontend" {
   count   = length(var.components)
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.selected.description]
 
   tags = {
     Name = var.components[count]
